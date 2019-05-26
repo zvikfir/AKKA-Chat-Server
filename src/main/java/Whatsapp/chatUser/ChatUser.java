039e939e6ba6aef3e931181dc03a4c9345b75131
Kfir Zvi <zvikfir10@gmail.com>
2019-05-26 17:35:55 +0300

    + connect working in chatuser

--- src/main/java/Whatsapp/chatUser/ChatUser.java
+++ src/main/java/Whatsapp/chatUser/ChatUser.java
@@ -4,18 +4,40 @@ import Whatsapp.managingServer.ManagingServer;
 import akka.actor.*;
 import akka.event.Logging;
 import akka.event.LoggingAdapter;
-import com.typesafe.config.Config;
+import akka.pattern.Patterns;
+import akka.remote.SystemMessageFormats;
+import akka.util.Timeout;
 import com.typesafe.config.ConfigFactory;
+import scala.concurrent.Await;
+import scala.concurrent.Future;
+import scala.concurrent.duration.Duration;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class ChatUser extends AbstractActor {
     LoggingAdapter log = Logging.getLogger(getContext().getSystem(), this);
 
     static public Props props(String username) {
         return Props.create(ChatUser.class, () -> new ChatUser(username));
+    }
+
+    public static class UserConnectSuccess {
+        String msg;
+
+        public UserConnectSuccess(String msg) {
+            this.msg = msg;
+        }
+    }
+
+    public static class UserConnectFailure {
+        String msg;
+
+        public UserConnectFailure(String msg) {
+            this.msg = msg;
+        }
     }
 
     public static class ChatMessage {
@@ -37,9 +59,36 @@ public class ChatUser extends AbstractActor {
     }
 
     String username;
+    final ActorSelection managingServer = getContext().actorSelection("akka://Whatsapp@127.0.0.1:2552/user/managingServer");
+    final Timeout timeout = new Timeout(Duration.create(1, TimeUnit.SECONDS));
 
     public ChatUser(String username) {
         this.username = username;
+    }
+
+    @Override
+    public void preStart() throws IOException {
+
+        do {
+            Scanner in = new Scanner(System.in);
+            System.out.print("Enter username:");
+            String username = in.nextLine();
+
+            Future<Object> rt = Patterns.ask(managingServer, new ManagingServer.Connect(username), timeout);
+            try {
+                Object result = Await.result(rt, timeout.duration());
+                if (result instanceof UserConnectSuccess) {
+                    this.username = username;
+                    System.out.println(((UserConnectSuccess) result).msg);
+                    return;
+                }
+                else {
+                    System.out.println(((UserConnectFailure) result).msg);
+                }
+            } catch (Exception e) {
+                System.out.println("server is offline!");
+            }
+        } while(true);
     }
 
     @Override
@@ -56,30 +105,12 @@ public class ChatUser extends AbstractActor {
 
     public static void main(String[] args) {
         final ActorSystem system = ActorSystem.create("chatUser", ConfigFactory.load("chatUser"));
-
         try {
-            Scanner in = new Scanner(System.in);
-            System.out.print("Enter username:");
-            String username = in.nextLine();
-            System.out.print("Username is - " + username);
-
-            ActorSelection managingServer = system.actorSelection("akka://Whatsapp@127.0.0.1:2552/user/managingServer");
-
-
             final ActorRef user =
                     system.actorOf(ChatUser.props("Yossi"), "user");
-
-            
-
-            managingServer.tell(new ManagingServer.Connect(username), user);
-
-            System.out.println(">>> Press ENTER to exit <<<");
-            System.in.read();
-
-        } catch (IOException ioe) {
+        } catch (Exception ioe) {
         } finally {
             system.terminate();
         }
-
     }
 }
